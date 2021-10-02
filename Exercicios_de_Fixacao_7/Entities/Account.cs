0a3106896e27d73ec7b6efbe810483f293bcd1ec
Walter <walter.carnage@gmail.com>
2021-10-02 14:03:09 -0300

    Criadas as classes, sem código

---
+++ Exercicios_de_Fixacao_7/Entities/Account.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios_de_Fixacao_7.Entities
+{
+    class Account
+    {
+
+    }
+}
---
+++ Exercicios_de_Fixacao_7/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios_de_Fixacao_7.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
