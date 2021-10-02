bf415af2c2b5b4683b4eb0b251603201a0326017
Walter <walter.carnage@gmail.com>
2021-10-02 14:47:22 -0300

    Implementação concluída. Sistema finalizado e testado.

--- Exercicios_de_Fixacao_7/Entities/Account.cs
+++ Exercicios_de_Fixacao_7/Entities/Account.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Exercicios_de_Fixacao_7.Entities.Exceptions;
+using System.Globalization;
 
 namespace Exercicios_de_Fixacao_7.Entities
 {
@@ -9,7 +10,7 @@ namespace Exercicios_de_Fixacao_7.Entities
     {
         public int Number { get; private set; }
         public string Holder { get; set; }
-        public double Balance { get; set; }
+        public double Balance { get; private set; }
         public double WithdrawLimit { get; set; }
 
         public Account(int number, string holder, double balance, double withdrawLimit)
@@ -35,7 +36,20 @@ namespace Exercicios_de_Fixacao_7.Entities
 
         public void Withdraw(double amount)
         {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("Withdraw limit exceeded.");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("Balance cannot stay negative.");
+            }
             Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return "New balance: $" + Balance.ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
--- Exercicios_de_Fixacao_7/Program.cs
+++ Exercicios_de_Fixacao_7/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using Exercicios_de_Fixacao_7.Entities;
+using Exercicios_de_Fixacao_7.Entities.Exceptions;
 
 namespace Exercicios_de_Fixacao_7
 {
@@ -6,7 +9,36 @@ namespace Exercicios_de_Fixacao_7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance:");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit:");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, holder, balance, withdrawLimit);
+
+                Console.Write("\nEnter amount for withdraw: ");
+                acc.Withdraw(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+                Console.WriteLine(acc);
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Account error: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unespected error: " + e.Message);
+            }
         }
     }
 }
